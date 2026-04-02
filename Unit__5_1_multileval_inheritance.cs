529e9fc2142893ac56d5a39c0b8761b9dc8ad91d
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-04-02 08:49:13 +0530

    Add files via upload

---
+++ Unit__5_1_multileval_inheritance.cs
@@ -0,0 +1,36 @@
+// Write a program to demonstrate use of  multileval inheritance
+
+using System; 
+    
+public class Animal
+{        
+	public void eat()
+	{ 
+		Console.WriteLine("Eating..."); 
+	}     
+}     
+public class Dog: Animal
+{         
+	public void bark()
+	{ 
+		Console.WriteLine("Barking..."); 
+	}     
+}     
+public class BabyDog : Dog
+{         
+	public void weep()
+	{ 
+		Console.WriteLine("Weeping..."); 
+	}     
+}     
+
+class TestInheritance2
+{         
+	public static void Main(string[] args)
+	{              
+		BabyDog d1 = new BabyDog();              
+		d1.eat();             
+		d1.bark();              
+		d1.weep();          
+	}      
+}
