62222c479b5f0cffea204c6adbea39f25218268a
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-04-02 08:51:13 +0530

    Add files via upload

---
+++ Unit_5_6_Try_catch_Final.cs
@@ -0,0 +1,25 @@
+// Write a program to demonstrate the use of try, catch and finally in C#
+
+using System; 
+ 
+public class ExExample
+{      
+	public static void Main(string[] args)
+	{          
+		try
+		{              
+			int a = 10;              
+			int b = 0;              
+			int x = a / b;
+		}          
+		catch (Exception e)
+		{ 
+			Console.WriteLine(e); 
+		}          
+		finally
+		{ 
+			Console.WriteLine("Finally block is executed"); 
+		}          
+		Console.WriteLine("Rest of the code");      
+	}  
+} 
